89b39753361b785ca80a0e3158ac937539b08aa4
Vatsal <saglanivatsal@gmail.com>
2017-03-30 21:22:52 +0530

    Added explore, profilr, and home pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'pages#index'
+
+  get 'pages/home' => 'pages#home'
+
+  get 'pages/profile' => 'pages#profile'
+
+  get 'pages/explore' => 'pages#explore'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
