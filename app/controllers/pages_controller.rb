962003b4a4314e599c5eb1272639c56a9b784516
Vatsal <saglanivatsal@gmail.com>
2017-04-06 09:14:10 +0530

    Post model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,7 @@ class PagesController < ApplicationController
   end
 
   def home
+    @posts = Post.all
   end
 
   def profile
@@ -14,8 +15,10 @@ class PagesController < ApplicationController
     else
       redirect_to root_path, :notice => "User not Found!!"
     end
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
   end
 
   def explore
+    @posts = Post.all
   end
 end
